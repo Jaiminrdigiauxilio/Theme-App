d477bef753651faf2c0ddce857138d2de067f018
Jaiminrdigiauxilio <153737984+Jaiminrdigiauxilio@users.noreply.github.com>
2024-01-08 11:49:53 +0530

    working version 0.2

--- Theme-App/Controllers/AllThemesVC.swift
+++ Theme-App/Controllers/AllThemesVC.swift
@@ -20,8 +20,6 @@ class AllThemesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let flowLayout = FlowLayout()
-        themeCollection.collectionViewLayout = flowLayout
     }
 
     func setBgFromUsrDefault() {
@@ -31,49 +29,21 @@ class AllThemesVC: UIViewController {
 }
 
 
-extension AllThemesVC: UICollectionViewDelegate, UICollectionViewDataSource {
+extension AllThemesVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        return 11
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.seeAllCollectionCellIdentifier, for: indexPath) as! SeeAllCell
-        cell.collectionImg.kf.setImage(with: URL(string: K.imgUrl2))
+        cell.collectionImg.kf.indicatorType = .activity
+        cell.collectionImg.layer.cornerRadius = 25
+        cell.collectionImg.layer.masksToBounds = true
+        cell.collectionImg.kf.setImage(with: URL(string: K.imgUrl2), placeholder: UIImage(named: "placeholder"))
         return cell
     }
-}
-
-
-class FlowLayout: UICollectionViewFlowLayout {
-    override init() {
-        super.init()
-        commonInit()
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width/2 - 18, height: 210)
     }
-    
-    required init?(coder: NSCoder) {
-        super.init()
-        commonInit()
-        fatalError("init(coder:) has not been implemented")
-        
-    }
-    private func commonInit() {
-        scrollDirection = .horizontal
-        minimumLineSpacing = 10
-        minimumInteritemSpacing = 10
-    }
-    
-    override var itemSize: CGSize {
-            set {
-                // Adjust the width and height based on your requirements
-                let newWidth = (collectionView!.bounds.width - minimumInteritemSpacing) / 2
-                super.itemSize = CGSize(width: newWidth, height: newValue.height)
-            }
-            get {
-                return super.itemSize
-            }
-        }
-    
-    
-    
 }
